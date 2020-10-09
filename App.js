ef0e560f1c90599ccc582d4c496be532113bae20
tylereikenberg <tylereikenberg8195@gmail.com>
2020-10-09 15:59:11 -0400

    change to  App.js

--- App.js
+++ App.js
@@ -43,7 +43,7 @@ const App: () => React$Node = () => {
               <Text style={styles.sectionTitle}>Step 1ne</Text>
               <Text style={styles.sectionDescription}>
                 Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
+                screen and then come back to see your edits. This is a brand new change!!!
               </Text>
             </View>
             <View style={styles.sectionContainer}>
